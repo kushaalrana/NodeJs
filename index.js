5b310be66a37a12ccd11b3885ca02d8ea0c1db5d
kushaalrana <ranakushaal@gmail.com>
2020-07-23 16:33:55 +0530

    Assignment 1

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const { write } = require('fs');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
+
 const hostname ='localhost';
 const port = 3000;
 
@@ -13,6 +16,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
+
+
 app.use(express.static(__dirname + '/public'));//takes the file from the public folder
 
 
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -6,7 +6,6 @@ const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-
 .all ((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -29,6 +28,25 @@ dishRouter.route('/')
 .delete((req,res,next) => {
 
     res.end('Deleting all the dishes to you!');
+});
+
+
+
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put((req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
 });
 
 module.exports = dishRouter;
